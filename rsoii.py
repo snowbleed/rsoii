5c6fd31fc45407ea1d4427f394615048570661ee
snowbleed <45135758+snowbleed@users.noreply.github.com>
2018-12-13 21:02:08 +0100

    Update rsoii.py

--- rsoii.py
+++ rsoii.py
@@ -7,7 +7,7 @@ import time
 TOKEN = 'NTE4NTUzMDU2NDg1MzEwNjMy.DuSdpw.jihEt-Ge5QXaU8LVKiLpHhzNg4c'
 
 client = commands.Bot(command_prefix = '-')
-status = ['Merry Christmas, Marines!']
+status = ['Calling quorum...📝']
 needsConfirmation = False
 announcementConfirm = False
 
@@ -44,16 +44,11 @@ async def displayembed():
 
 @client.command()
 async def cmds():
-    await client.say("**-cmds**: Display a list of commands.\n**-ping**: Check if the bot is responding quickly\n**-delete**: Deletes entered amount of messages. Must be more than 2 and less than 100 messages and the messages can not be over 2 weeks old. Format: `-delete <integer>`\n**-announce**: Make an announcement. Format: `-announce <text>`\n**-training**: Announce a training. Format: `-training <mandatory/optional> <placeid>`\n**-deploy**: Announce a sentry deployment. Format: `-deploy <placeid>`")
-
+    await client.say("**-cmds**: Display a list of commands.\n**-ping**: Check if the bot is responding quickly\n**-delete**: Deletes entered amount of messages. Must be more than 2 and less than 100 messages and the messages can not be over 2 weeks old. Format: `-delete <integer>`\n**-announce**: Make an announcement. Format: `-announce <text>`")
 @client.command()
 async def ping():
     await client.say('Pong!')
 
-@client.command()
-async def grr():
-    await client.say("What the fuck did you just fucking say about me, you little bitch? I’ll have you know I graduated top of my class in the Marine Security Guard Battalion, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You’re fucking dead, kiddo.")
-    
 @client.command(pass_context=True)
 async def announce(ctx, *args):
     if {'Commanding Officer', 'Executive Officer', 'Sentry In Charge'} & {role.name for role in ctx.message.author.roles}:
@@ -95,26 +90,6 @@ async def training(ctx, arg1, arg2):
             await client.say(output)
     else:
         await client.say('You lack permission to use this command')
-
-@client.command()
-async def snow():
-    await client.say("bleed")
-
-@client.command()
-async def frost():
-    await client.say("bleed")
-
-@client.command(pass_context=True)
-async def deploy(ctx, arg1):
-    if {'Commanding Officer', 'Executive Officer', 'Sentry In Charge'} & {role.name for role in ctx.message.author.roles}:
-        output = "**NOTICE:** A sentry/protection deployment order has been issued. All sentries are ordered to respond to https://www.roblox.com/games/" + arg1 + "/--  immediately.  @everyone"
-        await client.say('You are about to announce a deployment, if you are sure please say -confirm')
-        msg = await client.wait_for_message(author=ctx.message.author, content='-confirm')
-        await client.send_message(client.get_channel('518286886251397121'), output)
-        await client.say("Deployment announced.")
-        needsConfirmation = False
-    else:
-        await client.say("You lack permission to use this command")
 
 @client.command(pass_context=True)
 async def delete(ctx, amount=100):
