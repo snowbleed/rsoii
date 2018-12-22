6b70245a9a09ae43f28ac9ba0065b058a422a2f4
snowbleed <45135758+snowbleed@users.noreply.github.com>
2018-12-22 21:17:47 +0100

    Update rsoii.py

--- rsoii.py
+++ rsoii.py
@@ -146,7 +146,7 @@ async def bill(ctx, arg):
     else:
         await client.send_message(ctx.message.author, "To submit a bill you need to be a senator, if you have any questions or concerns please contact `snowbleed#7824`")
 @client.command(pass_context=True)
-async def samcmd(ctx, arg1, arg2)
+async def samcmd(ctx, arg1, arg2):
     server = client.get_server('467897785845284864')
     member = server.get_member(ctx.message.author.id)
     role = discord.utils.get(server.roles, name='Senator')
