fb4bd1f5c2a330f57d36d648564ecf52dc956cf5
snowbleed <45135758+snowbleed@users.noreply.github.com>
2018-12-28 07:11:26 +0100

    Update rsoii.py

--- rsoii.py
+++ rsoii.py
@@ -50,18 +50,21 @@ async def committees(ctx):
         await client.say(embed=client.cmteembed)
     else:
         return
-"""
+
 @client.command(pass_context=True)
 async def members(ctx):
-    server = client.get_server('467897785845284864')
-    role = discord.utils.get(server.roles, name='Senator')
-    x = 1
-    for member in server.members:
-        if role in member.roles:
-            await client.send_message(client.get_channel('525493778010734602'), "Senator #" + str(x) + ", " + member.mention)
-            x += 1
-            await asyncio.sleep(1)
-"""
+    if ctx.message.author.id == '147999751441219584'
+        server = client.get_server('467897785845284864')
+        role = discord.utils.get(server.roles, name='Senator')
+        x = 1
+        for member in server.members:
+            if role in member.roles:
+                await client.send_message(client.get_channel('525493778010734602'), "Senator #" + str(x) + ", " + member.mention)
+                x += 1
+                await asyncio.sleep(1)
+    else:
+        return
+
 
 @client.command()
 async def cmds():
